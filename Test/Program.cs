a710b96eb570c0be32aa76fadfb982ceedfa29c9
Dmitriy <123815654+Cabalette@users.noreply.github.com>
2023-04-14 15:17:17 +0300

    go

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcMovie.Data;
+using MvcMovie.Models;
+
 namespace MvcMovie
 {
     public class Program
@@ -5,11 +10,19 @@ namespace MvcMovie
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddDbContext<MvcMovieContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("MvcMovieContext") ?? throw new InvalidOperationException("Connection string 'MvcMovieContext' not found.")));
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
 
             var app = builder.Build();
+
+            using (var scope = app.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                SeedData.Initialize(services);
+            }
 
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
@@ -28,7 +41,7 @@ namespace MvcMovie
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=HelloWorld}/{action=Index}/{id?}");
+                pattern: "{controller=Home}/{action=Index}/{id?}");
 
             app.Run();
         }
